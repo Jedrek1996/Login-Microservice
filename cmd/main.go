4f5c7a8418e71ed8e1b3b6fc7bdb206b695e2d98
Jedrek Koh <jedrek.koh@foodpanda.com>
2023-02-07 23:46:05 +0800

    Established new user signup query and added database connection

--- cmd/main.go
+++ cmd/main.go
@@ -1,34 +1,59 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+
+	db "Microservice-Login/database/sqlc"
+	util "Microservice-Login/util"
+
+	_ "github.com/lib/pq"
 )
 
 var tpl *template.Template
+var queries *db.Queries
+var newDB *sql.DB
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://root:secret@localhost:5430/FoodPanda9?sslmode=disable"
+)
 
 func init() {
 	tpl = template.Must(template.ParseGlob("./templates/testTemplates/*"))
+
+	var err error
+	newDB, err = sql.Open(dbDriver, dbSource)
+
+	if err != nil {
+		log.Fatal("Error connecting to db:", err)
+	}
+
+	db.New(newDB)
+
+	fmt.Println("Database conncted")
 }
 
 func main() {
 	//CSS Files for testing
+	queries = db.New(newDB)
 	fs := http.FileServer(http.Dir("./templates"))
 	http.Handle("/templates/", http.StripPrefix("/templates/", fs))
 
 	http.HandleFunc("/", loginTest)
+	http.HandleFunc("/signup", signUpTest)
 	// http.HandleFunc("/welcome", cookies.Welcome)
 	// http.HandleFunc("/welcome", cookies.Refresh)
 	// http.HandleFunc("/welcome", cookies.Logout)
-	// start the server on port 8080
 	log.Fatal(http.ListenAndServe(":8080", nil))
-
 }
 
+// Login
 func loginTest(res http.ResponseWriter, req *http.Request) {
-	//Login Template
 	if req.Method == "GET" {
 		fmt.Println("Loggin test")
 
@@ -47,25 +72,40 @@ func loginTest(res http.ResponseWriter, req *http.Request) {
 	}
 }
 
+// Signup
 func signUpTest(res http.ResponseWriter, req *http.Request) {
-	//Login Template
 	if req.Method == "GET" {
 		fmt.Println("Signup test")
 
-		err := tpl.ExecuteTemplate(res, "Login.gohtml", "")
+		err := tpl.ExecuteTemplate(res, "Signup.gohtml", "")
 
 		if err != nil {
 			log.Fatalln(err)
 		}
 	} else if req.Method == "POST" {
-		userName := req.FormValue("loginUsername")
-		userPassword := req.FormValue("loginPassword")
+		signupFirstName := req.FormValue("signupFirstName")
+		signupLastName := req.FormValue("signupLastName")
+		signupUsername := req.FormValue("signupUsername")
+		signuPassword := req.FormValue("signuPassword")
+		signupEmail := req.FormValue("signupEmail")
+		signupMobile := req.FormValue("signupMobile")
+		//Validate credentials and store them in database
+		fmt.Println(signupFirstName, signupLastName, signupUsername, signuPassword, signupEmail, signupMobile)
+		//Change this
+		arg := db.CreateUserParams{
+			FirstName: util.RandomFirstNameGenerator(),
+			LastName:  util.RandomLastNameGenerator(),
+			UserName:  util.RandomUsernameGenerator(),
+			Email:     util.RandomEmailGenerator(),
+			Mobile:    util.RandomMobileGenerator(),
+		}
 
-		fmt.Println(userName)
-		fmt.Println(userPassword)
+		fmt.Println(arg)
 
+		userDetails, err := queries.CreateUser(context.Background(), arg)
+
+		if err != nil {
+			fmt.Println(err, userDetails)
+		}
 	}
 }
-
-//Create queries for login/ logout user
-//
