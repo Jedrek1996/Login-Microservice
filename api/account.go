73c3a4e9a3ef4dbb271e0daf82eb95b29935305c
Jedrek Koh <jedrek.koh@foodpanda.com>
2023-03-05 15:08:41 +0800

    Updated create user api

---
+++ api/account.go
@@ -0,0 +1,44 @@
+package api
+
+import (
+	db "Microservice-Login/database/sqlc"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type CreateUserRequest struct {
+	FirstName    string `json:"first_name" binding:"required"`
+	LastName     string `json:"last_name" binding:"required"`
+	UserName     string `json:"user_name" binding:"required"`
+	UserPassword string `json:"user_password" binding:"required"`
+	Email        string `json:"email" binding:"required"`
+	Mobile       int32  `json:"mobile" binding:"required"`
+}
+
+func (server *Server) createUser(ctx *gin.Context) {
+	var req CreateUserRequest
+
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	arg := db.CreateUserParams{
+		FirstName:    req.FirstName,
+		LastName:     req.LastName,
+		UserName:     req.UserName,
+		UserPassword: req.UserPassword,
+		Email:        req.Email,
+		Mobile:       req.Mobile,
+	}
+
+	account, err := server.store.CreateUser(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, account)
+
+}
--- api/server.go
+++ api/server.go
@@ -6,7 +6,26 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// Serves http request
 type Server struct {
 	store  *db.Store
 	router *gin.Engine
+}
+
+// Creates new http server and setup routing
+func newServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	router.POST("/test1", server.createUser)
+	server.router = router
+	return server
+}
+
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
 }
