aa6c9f997eddd9943f9aa02182e5c43d8d6a87a0
Jedrek Koh <jedrek.koh@foodpanda.com>
2023-03-29 17:21:00 +0800

    Added endpoint for retrieving user detail via username

--- api/server.go
+++ api/server.go
@@ -2,8 +2,8 @@ package api
 
 import (
 	db "Microservice-Login/database/sqlc"
-	"net/http"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -18,16 +18,19 @@ func NewServer(store *db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"http://localhost:3000"}
+	config.AllowHeaders = []string{"Content-Type"}
+	config.AllowMethods = []string{"OPTIONS, POST, GET, PUT, DELETE"}
+	config.AllowCredentials = true
+
+	router.Use(cors.New(config))
+
 	router.POST("/createUser", server.createUser)
 	router.POST("/userLogin", server.userLogin)
 	router.POST("/userLogout", server.userLogout)
+	router.POST("/getUserData", server.getUserDetail)
 	router.POST("/test", server.AuthCookieMiddleware(), server.TestCookie)
-
-	router.GET("/protected_route", server.AuthCookieMiddleware(), func(c *gin.Context) {
-		// This route is protected and can only be accessed by authenticated users
-		// If the middleware function returns an unauthorized error, this function will not be executed
-		c.JSON(http.StatusOK, gin.H{"message": "This is a protected route"})
-	})
 
 	server.router = router
 	return server
